60209534299d98a1e22e8d536d6bc80ce1886816
Ivan Barinov <ibarinov@wiley.com>
2016-08-19 12:34:36 +0300

    Add api-gateway

--- tv-shows-service/src/main/java/com/shows/TvShowsServiceApplication.java
+++ tv-shows-service/src/main/java/com/shows/TvShowsServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TvShowsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TvShowsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TvShowsServiceApplication.class, args);
+    }
 }
