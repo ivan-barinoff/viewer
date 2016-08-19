34057f910a9e3da081deb170114ad0c38024bd18
Ivan Barinov <ibarinov@wiley.com>
2016-08-19 12:16:14 +0300

    Initial configuration

---
+++ tv-shows-service/src/main/java/com/shows/TvShowsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.shows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvShowsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvShowsServiceApplication.class, args);
+	}
+}
