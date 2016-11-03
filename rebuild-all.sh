1f13318ad15a74c1f32f57deb5c850051cbface5
Ivan Barinov <ibarinov@wiley.com>
2016-11-03 15:51:09 +0300

    Add Docker support

---
+++ rebuild-all.sh
@@ -0,0 +1,16 @@
+cd config-server
+docker build -t config-server .
+cd ../api-gateway
+docker build -t api-gateway .
+cd ../discovery-service
+docker build -t discovery-service .
+cd ../movies-service
+docker build -t movies-service .
+cd ../rating-service
+docker build -t rating-service .
+cd ../monitoring
+docker build -t monitoring .
+cd ../trace-service
+docker build -t trace-service .
+cd ../tv-shows-service
+docker build -t tv-shows-service .
